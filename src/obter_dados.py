1a7afc009cc1122341d6fba414c76026fffe1516
Guilherme <none>
2025-02-11 17:53:21 -0300

    alterar csv (seperar por "," em vez de ";")

--- src/obter_dados.py
+++ src/obter_dados.py
@@ -2,13 +2,13 @@ import pandas as pd
 
 def obter_dados():
     df_A = pd.read_csv('data/matriculas.csv', 
-                    sep=';')
+                    sep=',')
     
     A = df_A.iloc[:, 1:].replace(0, -999).to_numpy()
     
     alunos = df_A['aluno']
     
     M = pd.read_csv('data/disciplinas.csv', 
-                    sep=';')
+                    sep=',')
     
     return A, M, alunos
